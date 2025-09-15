edc5910ce83c763f38a528955e280c655d5c9c95
thedude-tech <the.dugue@gmail.com>
2025-09-15 17:28:11 +0200

    dd

--- ui/MainMenu.cs
+++ ui/MainMenu.cs
@@ -3,35 +3,39 @@ using System;
 
 public partial class MainMenu : Control
 {
-    public override void _Ready()
-    {
-        GetNode<Button>("VBoxContainer/NewGameButton").Pressed += OnNewGamePressed;
-        GetNode<Button>("VBoxContainer/LoadGameButton").Pressed += OnLoadGamePressed;
-        GetNode<Button>("VBoxContainer/QuitButton").Pressed += OnQuitPressed;
-    }
+	public override void _Ready()
+	{
+		var newGameBtn = GetNode<Button>("VBoxContainer/NewGameButton");
+		var loadGameBtn = GetNode<Button>("VBoxContainer/LoadGameButton");
+		var quitBtn = GetNode<Button>("VBoxContainer/QuitButton");
 
-    private void OnNewGamePressed()
-    {
-        GetTree().ChangeSceneToFile("res://main.tscn");
-    }
+		newGameBtn.Pressed += OnNewGamePressed;
+		loadGameBtn.Pressed += OnLoadGamePressed;
+		quitBtn.Pressed += OnQuitPressed;
+	}
 
-    private void OnLoadGamePressed()
-    {
-        GD.Print("Attempting to load game...");
-        var saveManager = GetNodeOrNull<Node>("/root/SaveManager");
-        if (saveManager != null && saveManager.HasMethod("LoadGame"))
-        {
-            saveManager.Call("LoadGame");
-        }
-        else
-        {
-            GD.Print("SaveManager not found, starting new game.");
-        }
-        GetTree().ChangeSceneToFile("res://main.tscn");
-    }
+	private void OnNewGamePressed()
+	{
+		GetTree().ChangeSceneToFile("res://main.tscn");
+	}
 
-    private void OnQuitPressed()
-    {
-        GetTree().Quit();
-    }
+	private void OnLoadGamePressed()
+	{
+		GD.Print("Attempting to load game...");
+		var saveManager = GetNodeOrNull<Node>("/root/SaveManager");
+		if (saveManager != null && saveManager.HasMethod("LoadGame"))
+		{
+			saveManager.Call("LoadGame");
+		}
+		else
+		{
+			GD.Print("SaveManager not found, starting new game.");
+		}
+		GetTree().ChangeSceneToFile("res://main.tscn");
+	}
+
+	private void OnQuitPressed()
+	{
+		GetTree().Quit();
+	}
 }
