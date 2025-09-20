4f17f08f54a30fbd199382ac8de397871765128e
thedude-tech <the.dugue@gmail.com>
2025-09-20 12:55:24 +0200

    dd

--- Main.cs
+++ Main.cs
@@ -3,18 +3,18 @@ using System;
 
 public partial class Main : Node
 {
-    public override void _Ready()
-    {
-        var world = GetNode("World") as Node;
-        var player = GetNode("Player") as Node;
+	public override void _Ready()
+	{
+		var world = GetNode("World") as Node;
+		var player = GetNode("Player") as Node;
 
-        if (world != null && player != null && world.HasMethod("Initialize"))
-        {
-            world.Call("Initialize", player);
-        }
-        else
-        {
-            GD.PrintErr("World or Player missing, or Initialize() not found.");
-        }
-    }
+		if (world != null && player != null && world.HasMethod("Initialize"))
+		{
+			world.Call("Initialize", player);
+		}
+		else
+		{
+			GD.PrintErr("World or Player missing, or Initialize() not found.");
+		}
+	}
 }
